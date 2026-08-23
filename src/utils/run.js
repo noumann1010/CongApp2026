66765b649c72ccf91b42eacb7c8bc699acba0b20
noumann1010 <noumann1010@gmail.com>
2026-08-23 16:34:55 -0700

    sds

--- src/utils/run.js
+++ src/utils/run.js
@@ -1,4 +1,25 @@
 import sickness from listOfSicknesses.js;
 
 //code
-
+// sicknesses = {...}
+// Result = {...}
+// sickness.forEach(sickness =>				//for every symptom
+// 	count = 0						//the count begins at 0
+// 	sickness.symptoms.forEach(symptom) => {		//for every symptom of a sickness
+// 		if(userSymtomps.includes(symtoms)){	//if the user has this symptom
+// 			matchCount++;			//increase the count by 1
+// 		}
+// 	}
+// 	if(matchCount==0) {
+// prevention_tips=”you probably dont have this”
+// 	}
+// 	if(matchCount==3){
+// 	prevention_tips=”you have a lot of symptoms of __, you should see a doctor and (Get rest)”
+// 	}
+// 	results.push({
+// 		Sickness: sickness.name,
+// 		Matches: matchCount,
+// 		Prevention_tips: sickness.prevention_tips
+// 	})
+// }
+// Result = {[flu, 1, prevention_tips],[strep_throat, 2, preventions_tips]}
